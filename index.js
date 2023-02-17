1b03195fc8878371b7467748e2c919bba27b2834
kumankoishi.py <7iggif@naver.com>
2023-02-17 09:17:56 +0900

    feat(update): add update todo

--- index.js
+++ index.js
@@ -54,8 +54,25 @@ app.post(endpoint, (req, res) => {
   res.send(newTodo);
 });
 app.patch(endpoint, (req, res) => {
-  // TODO: Implement PATCH(UPDATE) method
-  res.send(data);
+  if (!req.body || !req.body.title) {
+    res.setHeader(406);
+    res.send();
+    return;
+  }
+
+  const patchId = data.todos.findIndex((todo) => req.body.id === todo.id);
+  if (patchId === -1) {
+    res.setHeader(404);
+    res.send();
+    return;
+  }
+
+  data.todos[patchId] = {
+    ...data.todos[patchId],
+    title: req.body.title,
+  };
+
+  res.send(data.todos[patchId]);
 });
 app.delete(endpoint, (req, res) => {
   // TODO: Implement DELETE method
